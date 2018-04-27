775c2d017f8fbce8f313026cb394a133d6107365
sergiohc <sergioh.c@live.com>
2018-04-27 15:04:45 -0300

    cust devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   Rails.application.routes.draw do
-    devise_for :users, :controllers => { registrations: 'registrations' }
+    devise_for :users, :controllers => { registrations: 'users/registrations' }
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
