e1815f97ddf69d0db30e4a32959baef4fce1c358
sergiohc <sergioh.c@live.com>
2018-05-03 22:46:48 -0300

    conf acess to graphQL_client api github and create token

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "action_cable/engine"
 Bundler.require(*Rails.groups)
 
 # Star Wars API example wrapper
-module SWAPI
+module GitHub
   class Application < Rails::Application
   end
   # Configure GraphQL endpoint using the basic HTTP network adapter.
@@ -29,11 +29,10 @@ module SWAPI
 
   # Fetch latest schema on init, this will make a network request
   Schema = GraphQL::Client.load_schema(HTTP)
-
   # However, it's smart to dump this to a JSON file and load from disk
   #
   # Run it from a script or rake task
-  #   GraphQL::Client.dump_schema(SWAPI::HTTP, "path/to/schema.json")
+  # GraphQL::Client.dump_schema(SWAPI::HTTP, "db/schema.json")
   #
   # Schema = GraphQL::Client.load_schema("path/to/schema.json")
 
